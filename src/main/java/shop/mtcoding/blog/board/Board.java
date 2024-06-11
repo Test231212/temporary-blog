c77ac2b8493cdeda4e9be77a18e733154e9563f6
test231212 <j910219@gmail.com>
2024-06-11 16:05:49 +0900

    테이블 생성, 테스트

---
+++ src/main/java/shop/mtcoding/blog/board/Board.java
@@ -0,0 +1,35 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import shop.mtcoding.blog.user.User;
+
+import java.sql.Timestamp;
+
+@NoArgsConstructor
+@Data
+@Table(name = "board_tb")
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String title;
+    private String content;
+    @ManyToOne(fetch =  FetchType.EAGER)
+    private User user;
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @Builder
+    public Board(Integer id, String title, String content, Timestamp createdAt) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.user = user;
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -0,0 +1,16 @@
+package shop.mtcoding.blog.board;
+
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@RequiredArgsConstructor
+@Repository
+public class BoardRepository {
+    private final EntityManager em;
+
+    public Board findById(int id) {
+        Board board = em.find(Board.class, id);
+        return board;
+    }
+}
